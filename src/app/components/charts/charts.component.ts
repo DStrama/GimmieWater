fc22f8d8fb92304f715e3963c894aef401311a2a
kczaplin <kasiaczaplinska29@gmail.com>
2021-05-24 11:08:04 +0200

    added charts

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DailyHumidity } from '../../model/dailyHumidity';
 import { SensorHumidityOfHourlyHumidity } from '../../model/sensorHumidityOfHourlyHumidity';
+import { SensorHumidityOfDailyHumidity } from '../../model/sensorHumidityOfDailyHumidity';
 import { HumidityControllerService } from '../../services/humidityController.service';
 import { Chart } from 'node_modules/chart.js';
+import { HourlyHumidity } from '../../model/hourlyHumidity'
 
 @Component({
   selector: 'app-charts',
@@ -12,51 +14,156 @@ import { Chart } from 'node_modules/chart.js';
 export class ChartsComponent implements OnInit {
 
   humidity: SensorHumidityOfHourlyHumidity[]; //tutaj może Array ale nwm
+  SENSORS_DATA: SensorHumidityOfHourlyHumidity[] = [];
+  SENSORS_DATA_DAILY: SensorHumidityOfDailyHumidity[] = [];
+  COLORS = [
+    "rgb(75, 192, 192)",
+    "rgb(213, 58, 152)",
+    "rgb(42, 195, 152)",
+    "rgb(254, 195, 71)",
+    "rgb(15, 2, 71)",
+  ];
+   CHARTS_CONTAINER_NAME = ".charts-container";
+   CHART_CANVAS_HEIGHT = 375;
+   CHART_CANVAS_WIDTH = 700;
+   state = {
+     chartObjects: []
+   };
+   //chartsContainerElement = document.getElementById("charts-container");
+
+
+   chartsInitHourly(parent, charts) {
+     // For each chart create chart element in DOM according to its time and moisture
+     return charts.map((chart, chartNumber) => {
+       return this.createSensorChart(
+         parent,
+         "Hourly "+chart.sensorName,
+         chart.time,
+         chart.moisture,
+         this.COLORS[chartNumber]
+       );
+     });
+   }
+
+      chartsInitDaily(parent, charts) {
+        // For each chart create chart element in DOM according to its time and moisture
+        return charts.map((chart, chartNumber) => {
+          return this.createSensorChart(
+            parent,
+            "Daily "+chart.sensorName,
+            chart.time,
+            chart.moisture,
+            this.COLORS[chartNumber]
+          );
+        });
+      }
+
+   createSensorChart(parent, name, time, moisture, color) {
+     // Creating container div for the chart
+     var chartContainerElement = document.createElement("div");
+     chartContainerElement.className = "chart-container";
+
+     // Creating chart canvas element which is used by Chart.js to draw our chart
+     var chartCanvasElement = document.createElement("canvas");
+     //chartCanvasElement.height = this.CHART_CANVAS_HEIGHT;
+     //chartCanvasElement.width = this.CHART_CANVAS_WIDTH;
+     chartContainerElement.style.cssText = "width: 500px;margin-left: 40px;margin-right: 40px;margin-bottom: 40px;";
+     //te dwie linijki dodane
+     //chartContainerElement.appendChild(chartCanvasElement);
+     //parent.appendChild(chartContainerElement);
+
+     // Creating actual chart with the help of Chart.js lib
+     var chartObj = new Chart(chartCanvasElement.getContext("2d"), {
+       type: "line",
+       data: {
+         labels: time,
+         datasets: [
+           {
+             label: name,
+             data: moisture,
+             fill: false,
+             borderColor: color,
+           },
+         ],
+       },
+       options: {
+         scales: {
+           yAxes: [
+             {
+               ticks: {
+                 beginAtZero: false,
+               },
+             },
+           ],
+         },
+       },
+     });
+
+     // Putting the elements into DOM (into charts-container)
+     chartContainerElement.appendChild(chartCanvasElement);
+     parent.appendChild(chartContainerElement);
+
+     // Returning chart object from Chart.js
+     return chartObj;
+   }
 
   constructor(private humidityService: HumidityControllerService) {}
   //constructor() { }
+
   ngOnInit(): void {
-    console.log("dupa");
     this.humidityService.getHourlyHumidityUsingGET().subscribe(data => {
-    this.humidity = data;
+    type MeasurementsList = {time: number[]; moisture: number[]; sensorName: string};
+    var myArray: MeasurementsList[] = [];
+    var chartsContainerElement = document.getElementById("charts-container");
+    //chartsContainerElement.style.cssText = "display: flex;flex-direction: row;flex-wrap: wrap;justify-content: center; margin-top: 40px;"
+    console.log(chartsContainerElement);
+
+    for(let i=0; i< data.length; i++){
+      this.SENSORS_DATA.push(data[i]);
+      //console.log(data[i]!.humidityList);
+      var measurements = [];
+      var time = [];
+      //var names = [];
+
+      for(let j=0; j<data[i]!.humidityList.length; j++){
+        //console.log(data[i]!.humidityList[j].humidityAverage);
+        measurements.push(data[i]!.humidityList[j].humidityAverage);
+        time.push(data[i]!.humidityList[j].hour);
+      }
+      myArray.push({time: time, moisture: measurements, sensorName: data[i]!.sensor.sensorId});
+     // nameList.push(data[i]!.sensor.sensorId)
+    }
+    //console.log(myArray);
+    //console.log(nameList);
+
+    this.state.chartObjects = this.chartsInitHourly(chartsContainerElement, myArray);
     });
 
-    var ctx = document.getElementById('myChart');
-    var myChart = new Chart('myChart', {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
+        this.humidityService.getDailyHumidityUsingGET().subscribe(data_daily => {
+        type MeasurementsListDaily = {time: number[]; moisture: number[]; sensorName: string};
+        var myArrayDaily: MeasurementsListDaily[] = [];
+        var chartsContainerElement = document.getElementById("charts-container2");
+        console.log(chartsContainerElement);
+
+        for(let i=0; i< data_daily.length; i++){
+          this.SENSORS_DATA_DAILY.push(data_daily[i]);
+          //console.log(data[i]!.humidityList);
+          var measurements = [];
+          var time = [];
+
+          for(let j=0; j<data_daily[i]!.humidityList.length; j++){
+            //console.log(data[i]!.humidityList[j].humidityAverage);
+            measurements.push(data_daily[i]!.humidityList[j].humidityAverage);
+            time.push(data_daily[i]!.humidityList[j].day);
+          }
+          myArrayDaily.push({time: time, moisture: measurements, sensorName: data_daily[i]!.sensor.sensorId});
         }
-    });
+
+        this.state.chartObjects = this.chartsInitDaily(chartsContainerElement, myArrayDaily);
+        });
+
   }
+
 
 
 }
